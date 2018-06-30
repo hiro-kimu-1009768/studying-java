dd9ffb9783ede0632255211f9bc6f0d40df6a106
kimuhiro2005 <tyuel.shigoto.2005@gmail.com>
2018-06-30 14:50:00 +0900

    20180630 WordCount class update

--- EnglishWordCount/src/main/java/com/exercise/repository/WordCount.java
+++ EnglishWordCount/src/main/java/com/exercise/repository/WordCount.java
@@ -1,37 +1,63 @@
 package com.exercise.repository;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class WordCount {
-	private static String fileName = "src/main/resources/sample.txt"; //読み込みファイル名
+	private static final String FILE_PATH = "src/main/resources/sample.txt"; //読み込みファイル名
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(
-				new FileReader(fileName));
-		TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			//１：ファイルから文字列を読み込む
 
-			String[] words = line.split("\\s");
-			//２：文字列から単語を抽出する
+		Map<String, Integer> treeMap = new TreeMap<>();
 
-			for (String s : words) {
-				if (!treeMap.containsKey(s)) {
-					treeMap.put(s, 1);
-				} else {
-					treeMap.put(s, treeMap.get(s).intValue() + 1);
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_PATH))) {
+			String oneLine;
+			while ((oneLine = bufferedReader.readLine()) != null) {
+				String[] wordsList = oneLine.split("\\s");
+				for (String word : wordsList) {
+					if (!treeMap.containsKey(word)) {
+						treeMap.put(word, 1);
+					} else {
+						treeMap.put(word, treeMap.get(word).intValue() + 1);
+					}
 				}
 			}
-			//３：マップに登録する
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
+		List<String> list = new ArrayList<>();
+		int maxLengthOfSpelling = 0;
+		for (String key : treeMap.keySet()) {
+			list.add(key);
+
+			if (maxLengthOfSpelling < key.length()) {
+				maxLengthOfSpelling = key.length();
+			}
 		}
-		for (String s : treeMap.keySet()) {
-			System.out.println(s + treeMap.get(s));
+
+		Collections.sort(list, (o1, o2) -> {
+			return -treeMap.get(o1) + treeMap.get(o2);
+		});
+
+		System.out.println("出現回数トップ10");
+		String format = "%-" + maxLengthOfSpelling + "s: %3d";
+		for (String word : list) {
+			int count = treeMap.get(word);
+			if (10 <= count) {
+				System.out.printf(format, word, count);
+				System.out.println();
+			}
+
+			//		for (String word : treeMap.keySet()) {
+			//			System.out.println(word + " : " + treeMap.get(word));
 		}
-		//４：マップから出力する
 	}
 }
