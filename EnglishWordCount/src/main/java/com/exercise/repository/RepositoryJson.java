2dc98b0e95490c12e5e1adbd1f050bcbb0e13345
kimuhiro2005 <tyuel.shigoto.2005@gmail.com>
2018-07-08 12:10:07 +0900

    20180708 json update

--- EnglishWordCount/src/main/java/com/exercise/repository/RepositoryJson.java
+++ EnglishWordCount/src/main/java/com/exercise/repository/RepositoryJson.java
@@ -1,17 +1,33 @@
 package com.exercise.repository;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
+import com.exercise.resources.EnglishWord;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class RepositoryJson {
+	private static final String OUTFILE_PATH = "src/main/resources/output.json"; //書き込みファイル名
+
 	public String getAllToJson() {
 		String json = null;
 		try {
+			File file = new File(OUTFILE_PATH);
 			WordCount wordCount = new WordCountImpl();
-			ObjectMapper objectMapper = new ObjectMapper();
-			json = objectMapper.writeValueAsString(wordCount.getEnglishWord());
-			System.out.println(json);
+			wordCount.getEnglishWord();
+			ObjectMapper objectMapper = new ObjectMapper()
+					.enable(SerializationFeature.INDENT_OUTPUT);
+			ArrayNode rootNode = objectMapper.createArrayNode();
+			for (Map.Entry<String, Integer> entory : wordCount.getEnglishWord().entrySet()) {
+				EnglishWord englishWord = new EnglishWord(entory.getKey(), entory.getValue());
+				rootNode.addPOJO(englishWord);
+				json = objectMapper.writeValueAsString(englishWord);
+				objectMapper.writeValue(file, rootNode);
+				System.out.println(json);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
